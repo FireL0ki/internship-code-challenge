457070b961e0c05d28a0aa8878fa046eb62c9c25
muell <muell758@d.umn.edu>
2022-02-18 18:49:44 -0600

    updates / notes / finalizing

--- README.md
+++ README.md
@@ -36,3 +36,50 @@ Create unit tests that validate's your program's output. Aim for ~85% test cover
 To submit this challenge, please edit the README file to include instructions on how to run the challenge. 
 
 After this, ZIP the challenge and submit it to the email that you received the challenge from. 
+
+
+## My Approach to the Challenge
+
+I started by making a list of comments at the top of the document to break down the tasks I need to do.
+Then I wrote out some pseudo code to roughly outline the logic the different parts of my program would follow.
+Then, I wrote out the code that made the most sense for my brain, and made alterations & adjustments from there for efficiency and readability.
+
+I spent a lot of time googling different functions in JavaScript, and refamiliarizing myself with concepts I learned, and learning new ways of approaching it.
+When I got stuck, I either spoke the code out loud to myself, or found other people to speak it out loud to.
+
+
+**It took me some time to figure out what the challenge wanted, in terms of language, structure, etc. 
+For example: whether the challenge wanted me to connect to the json file in a specific way. 
+I ended up deciding to simply put it in a variable in my .js file, so I could dive into the challenge.
+
+
+I started out a lot of for loops, but I ended up with a lot of repetitive and long code.
+I worked with trying to create useable functions, and after getting stuck on creating dynamic variable names, looked back at loops.
+I talked over this with a mentor, and she encouraged me to try to focus on cutting down the loops- so
+I decided to shift to exploring the forEach loop, and was able to use that to cut down on repetitive code
+through use of a variable (barnColor)
+
+When building my code for working on dividing the animals between more barns, my mentor pointed me toward looking at the slice() menthod,
+when I had been looking at splice(). After experimenting with them, and reading up on the documentation, I went with slice() because 
+I realized splice() manipulated the original array.
+
+One of the errors I ran into was with using slice(); on first read, I though the documentation was saying the first number was for start index,
+and the second was for an int / how many it should grab after the index. This led to my arrays not populating properly, and I was able to catch it
+after some troubleshooting, that the second number was meant to be for an end index.
+
+Much of my troubleshooting involved utilizing console.log() frequently at every step of the code, especially when I was having trouble
+pinning down where an error was occuring. I also frequently spoke my code out loud to myself, explaining step by step exactly what 
+I wanted it / expected it to do. I utilized Google as a resource, frequently using MDN Web Docs, and often reading other peoples' questions / responses
+on stack overflow.
+
+
+I wanted to continue, but needed to balance this work with my current coursework, so was unable to go further on even distribution (difference of no more than 1)-- 
+If I had more time, I was looking at approaching it by looking at remainders, at re-divvying animals each time there was overflow 
+(re-counting total animals, dividing out, and mathematically figuring out the best number of barns)
+
+
+Overall, I learned a ton! I discovered many new methods, learned even more about my code process approach, and gained a bunch of tools that I will take forward in my work.
+
+
+## To Run this Challenge:
+Run via terminal with command: node main.js
--- main.js
+++ main.js
@@ -30,108 +30,54 @@ let barnAnimalsArray =
     ]
 }
 
-/*  1. Iterate over array of objects (livestockArray) & pull the barn colors (green, blue, red, black, brown). Make each barn color into its own array.
-    2. Iterate over livestockArray. Where ["barn"] = color, retrieve the element in the value attached to the animal key.
-
-        a. check if barn{color} array has space ( is < 4 in size (.length() ), otherwise, create a new barn of that color and add the animal to it.) 
-            -- make sure to check other barn multiples of each color!
-            i. if ({color}Barn.length() >= 4) {
-                 let {color}Barn_2 = []
-                 {color}Barn_2.push("(animal)")
-            } else {
-                {color}Barn.push("(animal)")
-            }
-
-    3. Once animal distributions have occurred, check barns of each color to make sure there is not a difference in animal number (.length()) of more than 1 between them
-    4. How do I ouput this data as JSON again?
-*/
-
 let barnAnimals = barnAnimalsArray["livestock"];
 
-// console.log("TEST")
-// for (let i = 0; i <= barnAnimals.length; i ++) {
-//     console.log(barnAnimals[i].barn)
-// }
+// create object to store
+let animalsByColor = {};
+
+// iterate over barnAnimals array of objects, use barnColor variable to store the different barn colors as they are accessed
+// this loop will place all the animals in their correct color barn arrays
+barnAnimals.forEach( (animal) => {
+    // set the barnColor variable to the element in the barn property in the current line
+    const barnColor = animal.barn;
+     // if property exists in animalsByColor (the specified color), push animal to key/property -- spread operator to not destroy data
+    if (animalsByColor.hasOwnProperty(barnColor)) {
+        const existingAnimals = animalsByColor[barnColor];
+        animalsByColor[barnColor] = [...existingAnimals, animal.animal];
+    } else {
+        // if !exists,create new property, and push animal value to it
+        animalsByColor[barnColor] = [animal.animal];
+    }
+} ); 
 
 
-// create a barn for each favorite color
-let greenBarn = [];
-let blueBarn = [];
-let redBarn = [];
-let blackBarn = [];
-let brownBarn = [];
+//variable to store max number of animals per barn
+const maxAnimalNumber = 4;
+const JSONObject = [];
 
-for (let i = 0; i <= barnAnimals.length; i++) {
-    if (barnAnimals[i]?.barn == "Green") {
-        greenBarn.push(barnAnimals[i]?.animal)
+
+
+// iterate over animalsByColor object. For each barn, divide by 4 (max number animals) to find out how many total barns are needed
+// visual example of end goal: "Barn_Green_x":, ["animal", "animal", "animal"]
+for (const barnColor in animalsByColor) {
+    const animals = animalsByColor[barnColor];
+    // find out numberOfBarns needed -- divide total animals by (max) 4 // use .ceil to round up any leftovers/remainders to get total barns
+    const numberOfBarns = Math.ceil(animals.length / maxAnimalNumber);
+
+    // loop to create each barn needed (number of times dictated by numberOfBarns discovered in previous step)
+    for (let i = 1; i <= numberOfBarns; i++) {
+        // create new sortedBarn object, use slice to grab specific animals from original array and move them into a new barn
+        // slice(start index, end index) -- 
+        const sortedBarn = {};
+        // (i-1) * 4 is the equation that will start the index at the next correct spot for the next barn. (loop 1 needs to start at index 0, loop 2 needs to start at index 4, ...)
+        const startIndex = (i-1)*maxAnimalNumber;
+
+        // take sortedBarn object and set the barn property to the descriptive string (names the barn)
+        sortedBarn.barn = `Barn_${barnColor}_${i}`;
+        // take the animals in the sortedBarn object and slice up the array by increments of 4
+        // use slice to pull items from original array without manipulating array data
+        sortedBarn.animals = animals.slice(startIndex, startIndex + maxAnimalNumber);
+        JSONObject.push(sortedBarn);
     }
 }
-
-for (let i = 0; i <= barnAnimals.length; i++) {
-    if (barnAnimals[i]?.barn == "Blue") {
-        blueBarn.push(barnAnimals[i]?.animal)
-    }
-}
-
-for (let i = 0; i <= barnAnimals.length; i++) {
-    if (barnAnimals[i]?.barn == "Red") {
-        redBarn.push(barnAnimals[i]?.animal)
-    }
-}
-
-for (let i = 0; i <= barnAnimals.length; i++) {
-    if (barnAnimals[i]?.barn == "Black") {
-        blackBarn.push(barnAnimals[i]?.animal)
-    }
-}
-
-for (let i = 0; i <= barnAnimals.length; i++) {
-    if (barnAnimals[i]?.barn == "Brown") {
-        brownBarn.push(barnAnimals[i]?.animal)
-    }
-}
-
-
-
-let numberRedBarns = redBarn.length / 4;
-
-// can I dynamically create array names? Do I build an array of arrays and pull from there? Is this best done without a loop?
-for (let i = 1; i <= numberRedBarns.length; i ++) {
-    
-}
-
-// set up code to check the barn array lengths
-if (redBarn.length >= 4) {   
-}
-
-
-if (redBarn.length % 4 == 3) {
-}
-
-if (redBarn.length % 4 == 2) {
-}
-
-if (redBarn.length % 4 == 1) {
-}
-
-if (redBarn.length % 4 == 0) {
-}
-
-
-
-//make animal sorting into a function
-// function putAnimalsInMatchingBarn(color) {
-//     for (let i = 0; i <= barnAnimals.length; i++) {
-//         if (barnAnimals[i]?.barn == color) {
-//             //-- how do I change the barn name with a variable?
-//             colorBarn.push(barnAnimals[i]?.animal)
-//         }
-//     }
-//     console.log(`${color} Barn: ${greenBarn}`);
-// }
-
-// putAnimalsInMatchingBarn("Green");
-// putAnimalsInMatchingBarn("Blue");
-// putAnimalsInMatchingBarn("Red");
-// putAnimalsInMatchingBarn("Black");
-// putAnimalsInMatchingBarn("Brown");
+console.log(JSONObject);
